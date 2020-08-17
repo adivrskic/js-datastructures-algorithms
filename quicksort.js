a57b531b8323555fb473d6b9ab810d8d5724817c
Adi Vrskic <35346212+adivrskic@users.noreply.github.com>
2020-08-17 06:39:56 -0400

    Create quicksort.js

---
+++ quicksort.js
@@ -0,0 +1,9 @@
+const quickSort = (arr, left=0, right=arr.length-1) => {
+  if(left < right) {
+    let pivotIndex = pivot(arr, left, right);
+    quickSort(arr, left, pivotIndex-1);
+    quickSort(arr, pivotIndex+1, right);
+  }
+
+  return arr;
+}
