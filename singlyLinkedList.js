94bb6fc004b9cec7a0530a8021b0a91282eb2999
Adi Vrskic <35346212+adivrskic@users.noreply.github.com>
2020-08-19 20:17:23 -0400

    Update singlyLinkedList.js

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -15,7 +15,7 @@ class SinglyLinkedList {
   push(val) {
     let node = new Node(val);
 
-    if(!this.head) {
+    if (!this.head) {
       this.head = node;
       this.tail = this.head;
     } else {
@@ -28,18 +28,18 @@ class SinglyLinkedList {
   }
 
   pop() {
-    if(!this.head) return undefined;
+    if (!this.head) return undefined;
     let current = this.head;
     let newTail = current;
 
-    while(current.next) {
+    while (current.next) {
       newTail = current;
       current = current.next;
     }
     this.tail = newTail;
     this.tail.next = null;
     this.length -= 1;
-    if(this.length === 0) {
+    if (this.length === 0) {
       this.head = null;
       this.tail = null;
     }
@@ -47,14 +47,48 @@ class SinglyLinkedList {
   }
 
   shift() {
-    if(!this.head) return undefined;
+    if (!this.head) return undefined;
     let current = this.head;
     this.head = current.next;
     this.length -= 1;
-    if(this.length === 0) {
+    if (this.length === 0) {
       this.tail = null;
     }
     return current;
+  }
+
+  unshift(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+
+    return this;
+  }
+
+  get(idx) {
+    if (idx < 0 || idx >= this.length) return null;
+    let count = 0;
+    let current = this.head;
+    while (count < idx) {
+      current = current.next;
+      count++;
+    }
+
+    return current;
+  }
+
+  set(val, idx) {
+    let node = this.get(idx);
+    if (node === null) return false;
+    
+    node.val = val;
+    return true;
   }
 }
 
