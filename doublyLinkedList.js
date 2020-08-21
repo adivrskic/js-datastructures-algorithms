4a8c2f8ed7575d556785dc5d274e06ff039f1b3c
Adi Vrskic <35346212+adivrskic@users.noreply.github.com>
2020-08-21 19:03:28 -0400

    Update doublyLinkedList.js

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -15,7 +15,7 @@ class DoublyLinkedList {
 
   push(val) {
     let newNode = new Node(val);
-    if (this.length === 0) {
+    if (!this.head) {
       this.head = newNode;
       this.tail = newNode;
     } else {
@@ -25,6 +25,74 @@ class DoublyLinkedList {
     }
     this.length++;
     return this;
+  }
+
+  pop() {
+    if(this.length === 0) return undefined;
+    let oldTail = this.tail;
+    if(this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = oldTail.prev;
+      this.tail.next = null;
+      oldTail.prev = null;
+    }
+
+    this.length--;
+    return oldTail;
+  }
+
+  shift() {
+    if(this.length === 0) return undefined;
+    let oldHead = this.head;
+    if(this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = oldHead.next;
+      this.head.prev = null;
+      oldHead.next = null;
+    }
+    this.length--;
+    return oldHead;
+  }
+
+  unshift(val) {
+    let newNode = new Node(val);
+    if(this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  get(idx) {
+    if(idx < 0 || idx >= this.length) return null;
+    let node;
+    let counter;
+    if(idx <= this.length / 2) {
+      counter = 0;
+      node = this.head;
+      while(counter !== idx) {
+        node = node.next;
+        counter++;
+      }
+    } else {
+      counter = this.length - 1;
+      node = this.tail;
+      while(counter !== idx) {
+        node = node.prev;
+        counter--;
+      }
+    }
+
+    return node;
   }
 }
 
