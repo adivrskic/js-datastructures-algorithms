e4fb6fea6ca02b37d29fbef731e93e7f45860d34
Adi Vrskic <35346212+adivrskic@users.noreply.github.com>
2020-08-25 06:42:04 -0400

    Update maxBinaryHeap.js

--- maxBinaryHeap.js
+++ maxBinaryHeap.js
@@ -8,15 +8,58 @@ class MaxBinaryHeap {
 
     let index = this.vals.length - 1;
     let element = this.vals[index];
-    while(index > 0) {
+    while (index > 0) {
       let parentIndex = Math.floor((index - 1) / 2);
       let parent = this.vals[parentIndex];
-      if(element <= parent) break;
+      if (element <= parent) break;
 
       this.vals[parentIndex] = element;
       this.vals[index] = parent;
       index = parentIndex;
     }
+  }
+
+  remove() {
+    const max = this.vals[0];
+    const end = this.vals.pop();
+    if (this.vals.length > 0) {
+      this.vals[0] = end;
+
+      let index = 0;
+      let length = this.vals.length;
+      let element = this.vals[0];
+
+      while (true) {
+        let leftChildIndex = 2 * index + 1;
+        let rightChildIndex = 2 * index + 2;
+        let leftChild, rightChild;
+        let swap = null;
+
+        if (leftChildIndex < length) {
+          leftChild = this.vals[leftChildIndex];
+          if (leftChild > element) {
+            swap = leftChildIndex;
+          }
+        }
+
+        if (rightChildIndex < length) {
+          rightChild = this.vals[rightChildIndex];
+          if (
+            (swap === null && rightChild > element) ||
+            (swap !== null && rightChild > leftChild)
+          ) {
+            swap = rightChildIndex;
+          }
+        }
+
+        if (swap === null) break;
+        this.vals[index] = this.vals[swap];
+        this.vals[swap] = element;
+        index = swap;
+      }
+    }
+
+    return max;
   }
 }
 
