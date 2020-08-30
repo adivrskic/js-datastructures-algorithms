6c2c627fc98e37a3d34e13fb4495d9fd35b737d4
Adi Vrskic <35346212+adivrskic@users.noreply.github.com>
2020-08-30 11:58:55 -0400

    Update graph.js

--- graph.js
+++ graph.js
@@ -13,15 +13,85 @@ class Graph {
   }
 
   removeEdge(vertex1, vertex2) {
-    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(vertex => vertex !== vertex2);
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      (vertex) => vertex !== vertex2
+    );
 
-    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(vertex => vertex !== vertex1);
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      (vertex) => vertex !== vertex1
+    );
   }
 
   removeVertex(vertex) {
-    this.adjacencyList[vertex].forEach(element => this.removeEdge(vertex, element));
+    this.adjacencyList[vertex].forEach((element) =>
+      this.removeEdge(vertex, element)
+    );
 
     delete this.adjacencyList[vertex];
+  }
+
+  dftRecursive(start) {
+    let result = [];
+    let visited = {};
+    const adjacencyList = this.adjacencyList;
+
+    (function dft(vertex) {
+      if (!vertex) return null;
+      visited[vertex] = true;
+      result.push(vertex);
+      adjacencyList[vertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          return dft(neighbor);
+        }
+      });
+    })(start);
+
+    return result;
+  }
+
+  dftIterative(start) {
+    let stack = [start];
+    let result = [];
+    let visited = {};
+    let current;
+
+    visited[start] = true;
+
+    while (stack.length) {
+      current = stack.pop();
+      result.push(current);
+      this.adjacencyList[current].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          stack.push(neighbor);
+        }
+      });
+    }
+
+    return result;
+  }
+
+  bft(start) {
+    let queue = [start];
+    let result = [];
+    let visited = {};
+    let current;
+
+    visited[start] = true;
+
+    while (queue.length) {
+      current = queue.shift();
+      result.push(current);
+
+      this.adjacencyList[current].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          queue.push(neighbor);
+        }
+      });
+    }
+
+    return result;
   }
 }
 
