4d03242e769d722cc96d515b2149b9da21c3a4c6
Adi Vrskic <35346212+adivrskic@users.noreply.github.com>
2020-08-31 17:25:56 -0400

    Update dijkstra.js

--- dijkstra.js
+++ dijkstra.js
@@ -11,6 +11,60 @@ class WeightedGraph {
     this.adjacencyList[vertex1].push({ node: vertex2, weight });
     this.adjacencyList[vertex2].push({ node: vertex1, weight });
   }
+
+  dijkstra(start, finish) {
+    const nodes = new PriorityQueue();
+    const distances = {};
+    const previous = {};
+    let path = []; // to be returned
+    let smallest;
+
+    // build initial state
+    for(let vertex in this.adjacencyList) {
+      if(vertex === start) {
+        distances[vertex] = 0;
+        nodes.enqueue(vertex, 0);
+      } else {
+        distances[vertex] = Infinity;
+        nodes.enqueue(vertex, Infinity);
+      }
+
+      previous[vertex] = null;
+    }
+
+    // while there is something to visit
+    while(nodes.values.length) {
+      smallest = nodes.dequeue().val;
+      if(smallest === finish) {
+        // done
+        while(previous[smallest]) {
+          path.push(smallest);
+          smallest = previous[smallest];
+        }
+        break;
+
+      }
+      
+      if(smallest || distances[smallest] !== Infinity) {
+        for(let neighbor in this.adjacencyList[smallest]) {
+          let nextNode = this.adjacencyList[smallest][neighbor];
+          let candidate = distances[smallest] + nextNode.weight;
+          let nextNeighbor = nextNode.node;
+
+          if(candidate < distances[nextNeighbor]) {
+            // update new smallest distance to neighbor
+            distances[nextNeighbor] = candidate;
+            // updating previous
+            previous[nextNeighbor] = smallest;
+            // enqueue in pqueue with new priority
+            nodes.enqueue(nextNeighbor, candidate);
+          }
+        }
+      }
+    }
+
+    return path.contat(smallest).reverse();
+  }
 }
 
 class PriorityQueue {
